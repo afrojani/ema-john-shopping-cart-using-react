9f0eff40ed186e1d51e7670b6dae113460316133
afrojani <sultana.ani2019@gmail.com>
2022-10-10 20:30:56 -0500

    order page ADDED WITH REACT ROUTER DOM

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Cart.css'
-const Cart = ({ cart }) => {
+const Cart = ({ cart, clearCart, children }) => {
     let total = 0;
     let shipping = 0;
     let quantity = 0;
@@ -20,6 +20,9 @@ const Cart = ({ cart }) => {
             <p>Total Shipping: ${shipping}</p>
             <p>Tax: ${tax}</p>
             <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
+            <button onClick={clearCart} >Clear Cart</button>
+            {children}
+
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { addToDb, getStoredCart } from '../../utilities/fakedb';
+import { Link } from 'react-router-dom';
+import { addToDb, deleteShoppingCart, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -29,16 +30,28 @@ const Shop = () => {
     }, [products]);
 
     const handleAddToCart = (selectedProduct) => {
-        console.log(selectedProduct);
+        // console.log(selectedProduct);
         let newCart = [];
         const exists = cart.find(product => product.id === selectedProduct.id);
         if (!exists) {
             selectedProduct.quantity = 1;
             newCart = [...cart, selectedProduct];
-
         }
+
+        else {
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
+
         setCart(newCart);
         addToDb(selectedProduct.id);
+    }
+
+
+    const clearCart = () => {
+        setCart([]);
+        deleteShoppingCart();
     }
 
     return (
@@ -53,7 +66,11 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <Cart cart={cart}></Cart>
+                <Cart clearCart={clearCart} cart={cart}>
+                    <Link to='/orders'>
+                        <button>Review items</button>
+                    </Link>
+                </Cart>
             </div>
         </div>
     );
