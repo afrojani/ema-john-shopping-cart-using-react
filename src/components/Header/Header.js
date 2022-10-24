25fe8b2c15c71ba2eca8438dcb627f08bcb0d613
afrojani <sultana.ani2019@gmail.com>
2022-10-24 17:20:44 -0500

    firebase authentication added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/UserContext';
 import logo from '../../images/Logo.svg';
 import './Header.css';
 const Header = () => {
+    const { user, logOut } = useContext(AuthContext);
     return (
         <div>
             <nav className='header'>
@@ -12,6 +14,19 @@ const Header = () => {
                     <Link to="/orders">Orders</Link>
                     <Link to="/inventory">Inventory</Link>
                     <Link to="/about">About</Link>
+                    {
+                        user?.uid ?
+                            <button className='btn-logout' onClick={logOut}>Log Out</button>
+                            :
+
+                            <>
+                                <Link to="/login">Login</Link>
+                                <Link to="/signup">Sign up</Link>
+
+                            </>
+
+                    }
+
                 </div>
             </nav>
         </div>
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCCf-WAf5xVHatauVTfM20S2Qmnm_ZM4zg",
+    authDomain: "ema-john-simple-b6cfe.firebaseapp.com",
+    projectId: "ema-john-simple-b6cfe",
+    storageBucket: "ema-john-simple-b6cfe.appspot.com",
+    messagingSenderId: "250059995484",
+    appId: "1:250059995484:web:53a4950c5ab3fd1f05e999"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
